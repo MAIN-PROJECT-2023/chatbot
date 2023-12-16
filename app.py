0363e3a533f1b85ca7f003ebdc4a0ff5f8556441
Shiban.M <shibanm23@gmail.com>
2023-12-17 01:45:34 +0530

    user and bot message styled

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
+
 """ import firebase_admin
 from firebase_admin import auth, credentials, initialize_app
 import pyrebase """
